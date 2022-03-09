d1408f08af1e9168a525ee652f0e7334a9eea438
Yet <3302261172@qq.com>
2022-03-09 17:02:10 +0800

    master commit 2

--- src/main/java/com/yet/GitTest.java
+++ src/main/java/com/yet/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Heelo Git 3");
         System.out.println("Hello Git 4");
         System.out.println("Hot-fix first");
+        System.out.println("master test");
     }
 }
